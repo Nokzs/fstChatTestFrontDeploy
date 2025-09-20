ffe26e33e106c58f858cfff42f014786e6549696
Nokzs <77673271+Nokzs@users.noreply.github.com>
2025-09-20 15:54:36 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // tu peux garder dist ou build, peu importe
+  },
   server: {
     watch: {
       usePolling: true
