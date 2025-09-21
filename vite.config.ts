2af5ae2ea4ffc2e2a4e191a23330081763cafb68
nokzs <alexischapusot@gmail.com>
2025-09-21 07:20:56 +0000

    Update from https://github.com/Nokzs/testCiReact/commit/8f47ebdc00d287a81fc43d409b709053327323dc

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/fstChatTestFrontDeploy/',
   server: {
     watch: {
       usePolling: true
     }
   },
-  build:{
-    outDir:"build"
-  },
-
   test: {
     environment: 'jsdom',
     globals: true,
